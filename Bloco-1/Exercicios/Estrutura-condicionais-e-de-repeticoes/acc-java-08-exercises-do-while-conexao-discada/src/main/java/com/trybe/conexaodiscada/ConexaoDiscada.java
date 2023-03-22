89bc47648c2c95889e0ecae918e5dd46e9bdbb00
Caique Ferian <caiquegsf@gmail.com>
2023-03-22 15:46:23 -0300

    Exercicio 1 Dia 2, Classe que simula três tentativa de conexão discada usando Do While, se o numéro máximo for atingido ou se o retorno da função conectou for TRUE, o laço é parado

---
+++ Bloco-1/Exercicios/Estrutura-condicionais-e-de-repeticoes/acc-java-08-exercises-do-while-conexao-discada/src/main/java/com/trybe/conexaodiscada/ConexaoDiscada.java
@@ -0,0 +1,39 @@
+package com.trybe.conexaodiscada;
+
+import java.util.Random;
+
+/**
+ * Classe ConexaoDiscada.
+ *
+ */
+public class ConexaoDiscada {
+
+  /**
+   * Método principal.
+   *
+   */
+  public static void main(String[] args) {
+    // Seu código aqui
+    int counter = 0;
+    do {
+      boolean conexao = conectou();
+      if (conexao) {
+        break;
+      }
+      counter += 1;
+    } while (counter < 3);
+  }
+
+  // Chame esta função escrevendo `conectou()`
+  static boolean conectou() {
+    return conexaoDiscada.verdadeiroOuFalso();
+  }
+
+  static ConexaoDiscada conexaoDiscada = new ConexaoDiscada();
+
+  // Retorna verdadeiro ou falso
+  boolean verdadeiroOuFalso() {
+    return new Random().nextInt(2) == 1;
+  }
+
+}
